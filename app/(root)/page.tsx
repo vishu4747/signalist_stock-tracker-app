9b55def3520d848b73e24f2a5d288d742e5b1cbe
Vishal Kharade <vishalkharade04@gmail.com>
2025-10-26 21:15:57 +0530

    Implement header

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+ const page = () => {
+  return (
+    <div className="flex min-h-screen home-wrapper">
+      Home
+    </div>
+  )
+};
+
+export default page;
---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const NAV_ITEMS = [
+    {  href: "/", title: "Dashboard" },
+    {  href: "/search", title: "Search" },
+    {  href: "/watchlist", title: "Watchlist" },
+
+];
